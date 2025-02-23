373b96e6b929740c6af0fd10a345dbeedb8ede93
alep-m <165958475+alep-m@users.noreply.github.com>
2025-02-23 16:04:36 -0500

    implementacion para actualizar la lista del html con los nombres del arreglo

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let arregloNombres = [];
+let amigos = [];
 
 function agregarAmigo() {
     let nombre = document.querySelector("#amigo").value;
 
-    // parar de ejecutar si no hay nada
+    // interrumpir ejecucion si no hay nada
     if (nombre == "") {
         alert("Por favor, inserte un nombre.");
         return;
     }
 
-    // agregar nombre al arreglo
-    arregloNombres.push(nombre);
+    amigos.push(nombre);
     nombre = "";
+
+    actualizarLista();
+}
+
+// vacia la lista del html y la actualiza con el contenido del arreglo
+function actualizarLista() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(nombre => {
+        let text = document.createTextNode(nombre);
+        let item = document.createElement("li");
+        item.appendChild(text);
+        lista.appendChild(item);
+    });
 }
