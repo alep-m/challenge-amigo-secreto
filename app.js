b32e4ffef67023ef84f7b0376be9a05643a3ea5b
alep-m <165958475+alep-m@users.noreply.github.com>
2025-02-23 14:53:07 -0500

    se implementa funcion que maneja el click del boton Añadir

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arregloNombres = [];
+
+function agregarAmigo() {
+    let nombre = document.querySelector("#amigo").value;
+
+    // parar de ejecutar si no hay nada
+    if (nombre == "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    // agregar nombre al arreglo
+    arregloNombres.push(nombre);
+    nombre = "";
+}
